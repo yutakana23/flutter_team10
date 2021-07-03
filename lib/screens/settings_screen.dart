432a72cbe4ff98738597eabb34bd8713c52f40c2
Kiichi Itoh <jiyi.meta@gmail.com>
2021-07-03 18:07:19 +0900

    タブの作成

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 
+Map<String, String> settings = {
+  '名前': 'しょーま',
+  '地域': '滋賀県',
+  '科目': '数学',
+  '公開範囲': '友達まで',
+};
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
-  static const String id = 'Setting_Screen';
+  static const String id = 'exams_screen';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(child: Text('data'), color: Colors.red,),
+        ],
+      ),
+    );
   }
 }
+
+// class SettingTile extends StatelessWidget {
+
+//   final String title, trailing;
+//   SettingTile({
+//     required this.title, 
+//     required this.trailing,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       title: Text(title),
+//       trailing: Text(trailing),
+//     );
+//   }
+// }
