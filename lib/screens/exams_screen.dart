d40aa9ffeca7dd15fbd4e23a6c81e261fdf58baa
Yuta Imai <immoarii.27623@icloud.com>
2021-07-03 23:40:40 +0900

    模試のテンプレ完成

--- lib/screens/exams_screen.dart
+++ lib/screens/exams_screen.dart
@@ -22,33 +22,50 @@ class ExamsScreen extends StatelessWidget {
             child: Container(
               margin: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
-                color: Colors.red,
+                color: Colors.orangeAccent,
                 borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('右の選択肢から科目を選んでね',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Text('　<数学>',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
           Expanded(
-            flex: 5,
+            flex: 6,
             child: Container(
               margin: EdgeInsets.all(15.0),
               decoration: BoxDecoration(
                 color: Colors.red,
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Expanded(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text('グラフをここに',
+                  style: TextStyle(
+                    fontSize: 20.0,
                   ),
                 ),
               ),
             ),
           ),
           Expanded(
-            flex: 4,
+            flex: 3,
             child: Container(
               margin: EdgeInsets.all(15.0),
               decoration: BoxDecoration(
@@ -62,22 +79,47 @@ class ExamsScreen extends StatelessWidget {
                     flex: 1,
                     child: Container(
                       width: 200.0,
-                      margin: EdgeInsets.all(10.0),
+                      margin: EdgeInsets.all(8.0),
                       decoration: BoxDecoration(
-                        color: Colors.white
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: Center(
+                        child: Text('克服したい単元',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.0,
+                          ),
+                        ),
                       ),
-                      child: Text('おすすめです'),
                     ),
                   ),
                   Expanded(
                     flex: 3,
                     child: Container(
-                      margin: EdgeInsets.all(10.0),
+                      margin: EdgeInsets.all(8.0),
                       width: double.infinity,
                       decoration: BoxDecoration(
-                          color: Colors.white
+                          color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Text('おすすめ'),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('数学Ⅰ・A:',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                            ),
+                          ),
+                          Text(' 計算分野の問題が苦手なようです。これからは、グラフの活用や、式の利用などを中心に克服しましょう。',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ],
+                      ),
+
                     ),
                   ),
                 ],
