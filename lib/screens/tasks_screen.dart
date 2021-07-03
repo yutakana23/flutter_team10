4243c9701135efc1253f451e2712232b19db2e43
ShunyaMakino <shunyamakino@gmail.com>
2021-07-04 04:08:24 +0900

    UIの変更・通知数の実装

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,10 +7,11 @@ class Task {
   final String area;
   bool isDone;
 
-  Task({required this.name,
-    required this.subject,
-    required this.area,
-    this.isDone = false});
+  Task(
+      {required this.name,
+      required this.subject,
+      required this.area,
+      this.isDone = false});
 
   void _action() {
     isDone = !isDone;
@@ -65,22 +66,8 @@ class Entry {
 }
 
 const List<Entry> frame = <Entry>[
-  Entry(
-      '数学',
-      <Entry>[
-        Entry('数と式'),
-        Entry('2次関数'),
-        Entry('三角比')
-      ]
-  ),
-  Entry(
-      '国語',
-      <Entry>[
-        Entry('現代文'),
-        Entry('古文'),
-        Entry('漢文')
-      ]
-  ),
+  Entry('数学', <Entry>[Entry('数と式'), Entry('2次関数'), Entry('三角比')]),
+  Entry('国語', <Entry>[Entry('現代文'), Entry('古文'), Entry('漢文')]),
 ];
 
 class SuggestedTasks extends StatefulWidget {
@@ -98,27 +85,63 @@ class SuggestedTasksState extends State<SuggestedTasks> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:_DisplayFrame(frame),
+      body: _DisplayFrame(frame),
     );
   }
 
   Widget _buildFrame(Entry root) {
-    print(_DisplayTasks(_tasks).runtimeType);
-    if (root.children.isEmpty)
-      return ExpansionTile(
-        title: Text(root.title),
-        children: [_DisplayTasks(_tasks)],
+    if (root.children.isEmpty) {
+      var TasksArea = _tasks.where((_tasks) => _tasks.area == root.title);
+      return GestureDetector(
+        onTap: () {
+
+        },
+        child: ListTile(
+            title: Text(root.title),
+            trailing: (TasksArea.length > 0) ?
+            Stack(alignment: Alignment.center, children: [
+              Icon(
+                Icons.brightness_1,
+                color: Colors.red,
+              ),
+              Text(
+                TasksArea.length.toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ]):
+            Text('')
+        ),
       );
+    }
+
+
     return ExpansionTile(
       key: PageStorageKey<Entry>(root),
-      title: Text(root.title),
+      title: Text(
+        root.title,
+        style: TextStyle(
+          //fontFamily: ,
+        )
+      ),
+      trailing: (_tasks.every((_tasks) => _tasks.subject == root.title)) ?
+        Icon(
+          Icons.brightness_1,
+          color: Colors.red,
+          size: 10.0,
+        ):
+      Text(''),
       children: root.children.map(_buildFrame).toList(),
     );
   }
 
   Widget _DisplayFrame(List<Entry> frame) {
     return ListView.builder(
-      itemBuilder: (BuildContext context, int index) => _buildFrame(frame[index]),
+      itemBuilder: (BuildContext context, int index) =>
+          _buildFrame(frame[index]),
       itemCount: frame.length,
     );
   }
@@ -143,6 +166,7 @@ class SuggestedTasksState extends State<SuggestedTasks> {
   Widget _DisplayTasks(List<Task> tasks) {
     return ListView.builder(
         shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
         itemCount: tasks.length,
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (BuildContext context, int i) {
