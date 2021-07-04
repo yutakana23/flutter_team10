701d34301af7a568245287c8b3e47f2cdac87330
Kiichi Itoh <jiyi.meta@gmail.com>
2021-07-04 14:14:44 +0900

    アドバイスの追加

--- lib/components/advice_brain.dart
+++ lib/components/advice_brain.dart
@@ -1,6 +1,6 @@
 import '../components/advice.dart';
 
-class QuizBrain {
+class AdviceBrain {
   List<Advice> _adviceList = [
     Advice(subject: '物理', unit: '力学', advice: '「力の働く運動」分野の問題が苦手なようです。公式をしっかりと覚えて、活用できるようにしましょう。力学は図を書くことでわかりやすくなります。'),
     Advice(subject: '物理', unit: '波動', advice: '「光の反射と屈折」の分野が苦手なようです。全反射の条件や屈折率の違う媒質に入った時の光の進路を中心に勉強してください。')
