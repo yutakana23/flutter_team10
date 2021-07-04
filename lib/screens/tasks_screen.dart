c388592f01cb9b8473f6f774c53b447fec7afcd2
ShunyaMakino <shunyamakino@gmail.com>
2021-07-04 11:59:33 +0900

    画面遷移を追加

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
 
 class Task {
   final String name;
@@ -18,31 +17,12 @@ class Task {
   }
 }
 
-// class TaskList extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<TaskData>(
-//       builder: (context, taskData, child) {
-//         return ListView.builder(
-//           itemBuilder: (context, index) {
-//             final task = taskData.tasks[index];
-//             return TaskTile(
-//               title: task.name,
-//               isChecked: task.isDone,
-//               checkboxCallback: (checkboxState) {
-//                 taskData.updateTask(task);
-//               },
-//               longPressCallBack: () {
-//                 taskData.deleteTask(task);
-//               },
-//             );
-//           },
-//           itemCount: taskData.taskCount,
-//         );
-//       }
-//     );
-//   }
-// }
+List<Task> _tasks = [
+  Task(name: '問1', subject: '数学', area: '数と式'),
+  Task(name: '問2', subject: '数学', area: '数と式'),
+  Task(name: '問3', subject: '数学', area: '2次関数'),
+
+];
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({Key? key}) : super(key: key);
@@ -68,6 +48,7 @@ class Entry {
 const List<Entry> frame = <Entry>[
   Entry('数学', <Entry>[Entry('数と式'), Entry('2次関数'), Entry('三角比')]),
   Entry('国語', <Entry>[Entry('現代文'), Entry('古文'), Entry('漢文')]),
+  Entry('物理', <Entry>[Entry('力学'), Entry('電磁気')])
 ];
 
 class SuggestedTasks extends StatefulWidget {
@@ -76,12 +57,6 @@ class SuggestedTasks extends StatefulWidget {
 }
 
 class SuggestedTasksState extends State<SuggestedTasks> {
-  List<Task> _tasks = [
-    Task(name: '問1', subject: '数学', area: '数と式'),
-    Task(name: '問2', subject: '数学', area: '数と式'),
-    Task(name: '問3', subject: '数学', area: '2次関数')
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,49 +66,51 @@ class SuggestedTasksState extends State<SuggestedTasks> {
 
   Widget _buildFrame(Entry root) {
     if (root.children.isEmpty) {
-      var TasksArea = _tasks.where((_tasks) => _tasks.area == root.title);
+      var TasksArea =
+          _tasks.where((_tasks) => _tasks.area == root.title).toList();
+      print(TasksArea.runtimeType);
       return GestureDetector(
         onTap: () {
-
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NextPage(TasksArea),
+              ));
         },
         child: ListTile(
             title: Text(root.title),
-            trailing: (TasksArea.length > 0) ?
-            Stack(alignment: Alignment.center, children: [
-              Icon(
-                Icons.brightness_1,
-                color: Colors.red,
-              ),
-              Text(
-                TasksArea.length.toString(),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ]):
-            Text('')
-        ),
+            trailing: (TasksArea.length > 0)
+                ? Stack(alignment: Alignment.center, children: [
+                    Icon(
+                      Icons.brightness_1,
+                      color: Colors.red,
+                    ),
+                    Text(
+                      TasksArea.length.toString(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ])
+                : Text('')),
       );
     }
 
-
     return ExpansionTile(
       key: PageStorageKey<Entry>(root),
-      title: Text(
-        root.title,
-        style: TextStyle(
-          //fontFamily: ,
-        )
-      ),
-      trailing: (_tasks.every((_tasks) => _tasks.subject == root.title)) ?
-        Icon(
-          Icons.brightness_1,
-          color: Colors.red,
-          size: 10.0,
-        ):
-      Text(''),
+      title: Text(root.title,
+          style: TextStyle(
+              //fontFamily: ,
+              )),
+      trailing: (_tasks.every((_tasks) => _tasks.subject == root.title))
+          ? Icon(
+              Icons.brightness_1,
+              color: Colors.red,
+              size: 10.0,
+            )
+          : Text(''),
       children: root.children.map(_buildFrame).toList(),
     );
   }
@@ -145,66 +122,91 @@ class SuggestedTasksState extends State<SuggestedTasks> {
       itemCount: frame.length,
     );
   }
+}
 
-  // Widget _buildRow(Task task) {
-  //   return Card(
-  //       child: ListTile(
-  //         title: Text(task.name),
-  //         trailing: ElevatedButton(
-  //           child: Text('button'),
-  //           style: ElevatedButton.styleFrom(
-  //             primary: Colors.blue,
-  //             onPrimary: Colors.white,
-  //           ),
-  //           onPressed: () {
-  //             print('pressed');
-  //           },
-  //         ),
-  //       ));
-  // }
+class NextPage extends StatefulWidget {
+  NextPage(this.tasks);
+  final List<Task> tasks;
 
-  Widget _DisplayTasks(List<Task> tasks) {
-    return ListView.builder(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: tasks.length,
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext context, int i) {
-          final Task task = tasks[i];
-          return Dismissible(
-              key: Key(task.name),
-              direction: DismissDirection.endToStart,
-              onDismissed: (DismissDirection dir) {
-                setState(() => tasks.removeAt(i));
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('done'),
-                    action: SnackBarAction(
-                      label: 'UNDO',
-                      onPressed: () {
-                        setState(() => tasks.insert(i, task));
-                      },
-                    ),
-                  ),
-                );
-              },
-              background: Container(
-                  color: Colors.green,
-                  alignment: Alignment.centerRight,
-                  child: const Icon(Icons.thumb_up)),
-              child: ListTile(
+  @override
+  NextPageState createState() => NextPageState(tasks);
+}
+
+class NextPageState extends State<NextPage> {
+  NextPageState(this.tasks);
+  final List<Task> tasks;
+  bool _flag = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tasks[0].area),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: ListView.builder(
+            itemCount: tasks.length,
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (BuildContext context, int i) {
+              final Task task = tasks[i];
+              return CheckboxListTile(
                 title: Text(task.name),
-                trailing: ElevatedButton(
-                  child: Text('button'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue,
-                    onPrimary: Colors.white,
-                  ),
-                  onPressed: () {
-                    print('pressed');
-                  },
-                ),
-              ));
-        });
+                value: task.isDone,
+                onChanged: (bool? value) {
+                  setState(() {
+                    task.isDone = value!;
+                  });
+                },
+              );
+            }),
+      ),
+    );
   }
 }
+
+//   Widget _DisplayTasks(List<Task> tasks) {
+//     return ListView.builder(
+//         shrinkWrap: true,
+//         physics: NeverScrollableScrollPhysics(),
+//         itemCount: tasks.length,
+//         padding: const EdgeInsets.all(16.0),
+//         itemBuilder: (BuildContext context, int i) {
+//           final Task task = tasks[i];
+//           return Dismissible(
+//               key: Key(task.name),
+//               direction: DismissDirection.endToStart,
+//               onDismissed: (DismissDirection dir) {
+//                 setState(() => tasks.removeAt(i));
+//                 ScaffoldMessenger.of(context).showSnackBar(
+//                   SnackBar(
+//                     content: Text('done'),
+//                     action: SnackBarAction(
+//                       label: 'UNDO',
+//                       onPressed: () {
+//                         setState(() => tasks.insert(i, task));
+//                       },
+//                     ),
+//                   ),
+//                 );
+//               },
+//               background: Container(
+//                   color: Colors.green,
+//                   alignment: Alignment.centerRight,
+//                   child: const Icon(Icons.thumb_up)),
+//               child: ListTile(
+//                 title: Text(task.name),
+//                 trailing: ElevatedButton(
+//                   child: Text('button'),
+//                   style: ElevatedButton.styleFrom(
+//                     primary: Colors.blue,
+//                     onPrimary: Colors.white,
+//                   ),
+//                   onPressed: () {
+//                     print('pressed');
+//                   },
+//                 ),
+//               ));
+//         });
+//   }
+// }
